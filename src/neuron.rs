ab4f4627d1b0f5991cb4d67bc18b54386e26a0a0
Ludvig Lindholm <ludviglindholm127@gmail.com>
2025-10-24 20:21:04 +0200

    Refactoring constants, neurons and synapses

--- src/neuron.rs
+++ src/neuron.rs
@@ -1,30 +1,51 @@
+
 #[derive(Clone, Debug)]
 pub struct Neuron {
     pub id: usize,
     pub membrane_potential: f64,
     pub threshold: f64,
-    pub refractory_period: u32,
+    pub refractory_period: f32,
     pub exiting_synapses: Vec<usize>,
     pub entering_synapses: Vec<usize>,
-    pub layer: u8, // 0: input, 1: hidden, 2: output
+    pub time_since_fire: f32
 }
 
-impl Neuron {
+pub trait NeuronBehavior {
+    fn new(threshold: f64, id: usize) -> Self;
+    fn integrate(&mut self, input_current: f64);
+    fn is_firing(&self) -> bool;
+    fn reset(&mut self);
+    
+}
+
+impl NeuronBehavior for Neuron {
     /// Constructor to create a new neuron.
-    pub fn new(threshold: f64, id: usize) -> Self {
+    fn new(threshold: f64, id: usize) -> Self {
         Neuron {
             id,
             membrane_potential: 0.0,
             threshold,
-            refractory_period: 0,
+            refractory_period: 0.0,
+            time_since_fire: f32::INFINITY,
             exiting_synapses: Vec::new(),
             entering_synapses: Vec::new(),
-            layer: 0, // Default to input layer
         }
     }
 
-    pub fn reset(&mut self) {
+    fn integrate(&mut self, input_current: f64) {
+        if (self.refractory_period <= 0.0) {
+            self.membrane_potential += input_current;
+        } else {
+            self.refractory_period -= 1.0;
+        }
+    }
+
+    fn is_firing(&self) -> bool {
+        self.membrane_potential >= self.threshold
+    }
+
+    fn reset(&mut self) {
         self.membrane_potential = 0.0;
-        self.refractory_period = 0;
+        self.refractory_period = 0.0;
     }
 }
--- src/synapse.rs
+++ src/synapse.rs
@@ -1,11 +1,12 @@
 use rand::Rng;
+use crate::constants::{MAX_SYNAPSE_DELAY_MS, MIN_SYNAPSE_DELAY_MS};
 
 pub trait Synapse: Send + Sync {
     fn get_presynaptic_neuron(&self) -> usize;
     fn get_postsynaptic_neuron(&self) -> usize;
-    fn get_weight(&self) -> f64;
-    fn get_delay(&self) -> u32;
-    fn update_weight(&mut self, delta_w: f64);
+    fn get_weight(&self) -> f32;
+    fn get_delay(&self) -> f32;
+    fn update_weight(&mut self, delta_w: f32);
     fn clone_box(&self) -> Box<dyn Synapse>;
 }
 
@@ -19,23 +20,18 @@ impl Clone for Box<dyn Synapse> {
 pub struct ChemicalSynapse {
     pub presynaptic_neuron: usize,
     pub postsynaptic_neuron: usize,
-    pub weight: f64,
-    pub delay: u32,
+    pub weight: f32,
+    pub delay: f32, // in milliseconds
 }
 
 impl ChemicalSynapse {
-    pub fn new(presynaptic_neuron: usize, postsynaptic_neuron: usize, weight: f64) -> Self {
+    pub fn new(presynaptic_neuron: usize, postsynaptic_neuron: usize) -> Self {
         let mut rng = rand::thread_rng();
         ChemicalSynapse {
             presynaptic_neuron,
             postsynaptic_neuron,
-            // If weight is 1.0 or -1.0, it's likely an initial setup call, not a random one.
-            weight: if weight == 1.0 || weight == -1.0 {
-                weight
-            } else {
-                rng.gen_range(-0.5..0.5)
-            },
-            delay: rng.gen_range(1..=5),
+            weight: rng.gen_range(0.0..1.0),
+            delay: rng.gen_range(MIN_SYNAPSE_DELAY_MS..MAX_SYNAPSE_DELAY_MS),
         }
     }
 }
@@ -49,15 +45,15 @@ impl Synapse for ChemicalSynapse {
         self.postsynaptic_neuron
     }
 
-    fn get_weight(&self) -> f64 {
+    fn get_weight(&self) -> f32 {
         self.weight
     }
 
-    fn get_delay(&self) -> u32 {
+    fn get_delay(&self) -> f32 {
         self.delay
     }
 
-    fn update_weight(&mut self, delta_w: f64) {
+    fn update_weight(&mut self, delta_w: f32) {
         self.weight += delta_w;
     }
 
